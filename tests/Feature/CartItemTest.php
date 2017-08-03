40783623f94b82cb75b1a464478a32a141dcc374
Alex <alexnezhynsky@gmail.com>
2017-08-02 22:15:00 -0400

    Feature finalized

--- tests/Feature/CartItemTest.php
+++ tests/Feature/CartItemTest.php
@@ -39,4 +39,24 @@ class CartItemTest extends TestCase
             'quantity' => $productQuantity
         ]);
     }
+
+    public function testDoesNotAddItemIfCartNotFound()
+    {
+        // Given
+        $cartUuid = 'a-random-non-existent-cart-uuid';
+        $product = factory(Product::class)->create();
+        $productQuantity = 1;
+
+        // When
+        $response = $this->postJson(
+            route('cart-items.store', ['cart' => $cartUuid]), 
+            [
+                'product_id' => $product->id,
+                'quantity' => $productQuantity
+            ]
+        );
+
+        // Then
+        $response->assertStatus(404);
+    }
 }
