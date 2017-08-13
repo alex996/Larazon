631d2df715129fffd60a313a94df2356a6d027bb
Alex <alexnezhynsky@gmail.com>
2017-08-12 21:22:27 -0400

    Cleanup

--- app/Policies/AddressPolicy.php
+++ app/Policies/AddressPolicy.php
@@ -12,18 +12,6 @@ class AddressPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view the address.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Address  $address
-     * @return mixed
-     */
-    public function view(User $user, Address $address)
-    {
-        return $user->owns($address);
-    }
-
-    /**
      * Determine whether the user can delete the address.
      *
      * @param  \App\Models\User  $user
