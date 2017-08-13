945cf6931e7610a7aa192b996bb6d421cdb38b61
Alex <alexnezhynsky@gmail.com>
2017-08-12 21:22:36 -0400

    Started billing

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Card;
+use Illuminate\Http\Request;
+
+class CardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request. [
+            'token' => 'required|string|max:255'
+        ]);
+
+        $user = $request->user;
+
+        if ($user->hasStripeId()) {
+
+        } else {
+            $customer = $user->createAsStripeCustomer(
+                $request->token
+            );
+        }
+
+        
+        
+
+        // Save the card with address
+        // 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Card  $card
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Card $card)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Card  $card
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Card $card)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Card  $card
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Card $card)
+    {
+        //
+    }
+}
