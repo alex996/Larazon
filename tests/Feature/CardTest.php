aac047055493289d2eb1b907b9865f81b9a84346
Alex <alexnezhynsky@gmail.com>
2017-08-13 23:27:00 -0400

    More granular tests

--- tests/Feature/CardTest.php
+++ tests/Feature/CardTest.php
@@ -17,7 +17,6 @@ class CardTest extends TestCase
             $user = factory(User::class)->create()
         );
         $stripeToken = $this->getStripeToken();
-        $stripeCard = $stripeToken->card;
 
         // When
         $response = $this->postJson(route('cards.store'), [
@@ -26,36 +25,14 @@ class CardTest extends TestCase
             'Authorization' => 'Bearer '.$jwtToken
         ]);
         $user = $user->fresh();
-        $card = $user->cards()->first();
 
         // Then
         $response->assertStatus(201);
 
         $this->assertTrue($user->hasStripeId());
         $this->assertTrue($user->hasCardOnFile());
-        $this->assertNotEmpty($card);
         
-        $this->assertDatabaseHas('cards', [
-                'stripe_id' => $stripeCard->id,
-                'user_id' => $user->id,
-                'brand' => $stripeCard->brand,
-                'last4' => $stripeCard->last4,
-                'country' => $stripeCard->country,
-                'exp_month' => $stripeCard->exp_month,
-                'exp_year' => $stripeCard->exp_year,
-                'default' => true
-            ])
-            ->assertDatabaseHas('addresses', [
-                'addressable_id' => $card->id,
-                'addressable_type' => 'cards',
-                'line_1' => $stripeCard->address_line1,
-                'line_2' => $stripeCard->address_line2,
-                'city' => $stripeCard->address_city,
-                'state' => $stripeCard->address_state,
-                'country' => $stripeCard->address_country,
-                'zip' => $stripeCard->address_zip,
-            ]);
-
+                
         // 1. it creates Stripe customer
         // 2. it saves stripe_id on user
         // 3. it adds card to Stripe customer
---
+++ tests/Unit/Models/Billing/HasCreditCardsTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit\Models\Billing;
+
+use Tests\TestCase;
+use App\Models\{Card, User};
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class HasCreditCardsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    public function testItHasCards()
+    {
+        $this->user->cards()->saveMany(
+            factory(Card::class, 5)->make()
+        );
+
+        $this->assertInstanceOf(Collection::class, $this->user->cards);
+
+        $this->assertInstanceOf(Card::class, $this->user->cards->first());
+    }
+
+    public function testItAddsStripeCardToUser()
+    {
+        // Given
+        $token = $this->getStripeToken();
+        $stripeCard = $token->card;
+        // When
+        $this->user->addCard($token->card);
+        $card = $this->user->cards()->first();
+        // Then
+        $this->assertNotEmpty($card);
+        $this->assertDatabaseHas('cards', [
+            'stripe_id' => $stripeCard->id,
+            'user_id' => $this->user->id,
+            'brand' => $stripeCard->brand,
+            'last4' => $stripeCard->last4,
+            'country' => $stripeCard->country,
+            'exp_month' => $stripeCard->exp_month,
+            'exp_year' => $stripeCard->exp_year,
+            'default' => true
+        ])
+        ->assertDatabaseHas('addresses', [
+            'addressable_id' => $card->id,
+            'addressable_type' => 'cards',
+            'line_1' => $stripeCard->address_line1,
+            'line_2' => $stripeCard->address_line2,
+            'city' => $stripeCard->address_city,
+            'state' => $stripeCard->address_state,
+            'country' => $stripeCard->address_country,
+            'zip' => $stripeCard->address_zip,
+        ]);
+    }
+}
